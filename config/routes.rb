80c727dfb9d45140f28775ca4c0abaeb25565b49
Mugunthan Raju <mugunthanraju2000@gmail.com>
2022-05-10 23:39:33 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'application#hello'
-
 end
